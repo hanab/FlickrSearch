f124003cc46f9f77c532e673758c7128d0ba93c4
Hana Demas <hb16826@gmail.com>
2018-08-02 10:43:49 +0200

    detail view controller added

---
+++ FlickrSearch/Controllers/PhotoDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoDetailViewController.swift
+//  FlickrSearch
+//
+//  Created by Hana  Demas on 8/2/18.
+//  Copyright © 2018 ___HANADEMAS___. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    var selectedPhoto: FlickrPhoto?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
