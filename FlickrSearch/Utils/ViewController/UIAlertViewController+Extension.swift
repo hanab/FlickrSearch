e55eb527459be4721a4f9a28124acad72af0230d
Hana Demas <hb16826@gmail.com>
2018-08-02 10:25:04 +0200

    alert view to display error message added

---
+++ FlickrSearch/Utils/ViewController/UIAlertViewController+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIAlertViewController+Extension.swift
+//  FlickrSearch
+//
+//  Created by Hana  Demas on 8/2/18.
+//  Copyright © 2018 ___HANADEMAS___. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func displayAlertWith(title: String, message: String, style: UIAlertControllerStyle = .alert) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        let action = UIAlertAction(title: title, style: .default) { (action) in
+            self.dismiss(animated: true, completion: nil)
+        }
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
