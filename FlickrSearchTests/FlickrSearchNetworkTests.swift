17936ab074f6877b7ef6ccefae6991271c9b7020
Hana Demas <hb16826@gmail.com>
2018-08-05 17:16:48 +0200

    network layer test with mock

--- FlickrSearchTests/FlickrSearchNetworkTests.swift
+++ FlickrSearchTests/FlickrSearchNetworkTests.swift
@@ -9,28 +9,54 @@
 import XCTest
 @testable import FlickrSearch
 
-class FlickrSearchTests: XCTestCase {
+class FlickrSearchNetworkTests: XCTestCase {
+    
+    //MARK: Properties
+    var flickrApiClient:FlickrAPIClient!
+    var session = MockURLSession()
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        flickrApiClient = FlickrAPIClient(session: session)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    //MARK: TestCases
+    func testDataTaskResumeCalled() {
+        let dataTask = MockURLSessionDataTask()
+        session.mockDataTask = dataTask
+        
+        flickrApiClient.fetchSearchedPhotos(for: "dog",  completion: {(result) in
+            //nothing to do with the results for this test
+        })
+        XCTAssert(dataTask.resumeWasCalled)
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testDataTaskReturnPhotos() {
+        let dataTask = MockURLSessionDataTask()
+        session.mockDataTask = dataTask
+        
+        guard let url = URL(string: "https://myurl") else {
+            fatalError("URL can't be empty")
         }
+        
+        flickrApiClient.fetchFlickerPhotos(url: url, completion: {_ in
+            //nothing to do with the results for this test
+        })
+        
+        XCTAssert((session.mockURL == url))
     }
     
+    func testSuccessfulResponse() {
+        let data = "{}".data(using: .utf8)
+        session.mockData = data
+        
+        let url = URL(fileURLWithPath: "url")
+        var actualData: PhotosResult?
+        
+        flickrApiClient.fetchFlickerPhotos(url: url, completion: {(result) in
+            actualData = result
+        })
+        
+        XCTAssertNotNil(actualData)
+    }
 }
